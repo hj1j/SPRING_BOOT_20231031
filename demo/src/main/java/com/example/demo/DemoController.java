992e5d01a62c1e47de9fb07833bd46ee4111dac1
admin <admin>
2024-10-02 11:24:40 +0900

    24.10.02

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -11,9 +11,18 @@ public class DemoController {
         model.addAttribute("data", " 방갑습니다."); // model 설정
         return "hello"; // hello.html 연결
     }
-    /*@GetMapping("/about_detailed") // 전송 방식 GET
-    public String hello(Model model) {
-        model.addAttribute("data", " 방갑습니다."); // model 설정
-        return "hello"; // hello.html 연결
-    }*/
+    @GetMapping("/about_detailed") // 전송 방식 GET
+    public String about() {
+        return "about_detailed"; 
+    }
+    @GetMapping("/thymeleaf_test1")
+    public String thymeleaf_test1(Model model) {
+        model.addAttribute("data1", "<h2> 방갑습니다 </h2>");
+        model.addAttribute("data2", "태그의 속성 값");
+        model.addAttribute("link", 01);
+        model.addAttribute("name", "홍길동");
+        model.addAttribute("para1", "001");
+        model.addAttribute("para2", 002);
+    return "thymeleaf_test1";
+    }
 }
