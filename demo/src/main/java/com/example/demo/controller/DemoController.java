359d6ec149061e738b175a2172c44992e256f508
admin <yooan_u@naver.com>
2024-10-16 11:34:19 +0900

    4주차 완료, 5주차 블로그 게시판 작성중 +4주차 연습문제 진행중

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,8 +1,13 @@
-package com.example.demo;
+package com.example.demo.controller;
 
+import com.example.demo.model.service.TestService; // 최상단 서비스 클래스 연동 추가
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.model.domain.TestDB;
 
 @Controller // 컨트롤러 어노테이션 명시
 public class DemoController {
@@ -23,6 +28,18 @@ public class DemoController {
         model.addAttribute("name", "홍길동");
         model.addAttribute("para1", "001");
         model.addAttribute("para2", 002);
-    return "thymeleaf_test1";
+        return "thymeleaf_test1";
     }
+
+    @GetMapping("/testdb")
+    public String getAllTestDBs(Model model) {
+        TestDB test = testService.findByName("홍길동");
+        model.addAttribute("data4", test);
+        System.out.println("데이터 출력 디버그 : " + test);
+        return "testdb";
+    }
+
+    @Autowired 
+    TestService testService; // DemoController 클래스 아래 객체 생성
+    
 }
